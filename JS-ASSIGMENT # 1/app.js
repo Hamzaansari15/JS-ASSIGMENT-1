e79eb4edd6abcd85c7daf08b8c289f628b3bffaf
Hamzaansari <hamzaansaree15@gmail.com>
2022-05-31 13:44:46 -0700

    JS File Completed

---
+++ JS-ASSIGMENT # 1/app.js
@@ -0,0 +1,17 @@
+alert("Error!Please enter valid password");
+alert("Welcome to JS Land... \n Happy Coding!");
+alert("Welcome to JS land...");
+alert("Happy Coding!");
+alert("Hello...i can run JS file through web console");
+var myname = "Hamza Ansari";
+alert(myname);
+var message = "Hello World!";
+alert(message)
+alert("Hamza Ansari");
+alert("17 years");
+alert("Certified mobile application developer");
+alert("my email is hamzaansaree15@gmail.com");
+alert("Pizza \n Pizz \n Piz \n Pi \n p");
+var a = "yeah! I can wright HTML content through Javascript";
+document.write (a);
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
